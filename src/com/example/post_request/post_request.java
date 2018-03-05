024182072681b0bd5618f31d9c253f6f571841d1
max <r>
2018-03-06 00:12:40 +0300

    fix and blank

--- src/com/example/post_request/post_request.java
+++ src/com/example/post_request/post_request.java
@@ -11,29 +11,29 @@ public class post_request {
 
 
 
-        public static void main(String[] args) {
-
-            System.out.println("Hello,World - ");
-            HttpClient httpclient = HttpClients.createDefault();
-            HttpPost httppost = new HttpPost("http://www.a-domain.com/foo/");
-
-// Request parameters and other properties.
-            List<NameValuePair> params = new ArrayList<NameValuePair>(2);
-            params.add(new BasicNameValuePair("param-1", "12345"));
-            params.add(new BasicNameValuePair("param-2", "Hello!"));
-            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
-
-//Execute and get the response.
-            HttpResponse response = httpclient.execute(httppost);
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) {
-                InputStream instream = entity.getContent();
-                try {
-                    // do something useful
-                } finally {
-                    instream.close();
-                }
-            }
-        }
+//        public static void main(String[] args) {
+//
+//            System.out.println("Hello,World - ");
+//            HttpClient httpclient = HttpClients.createDefault();
+//            HttpPost httppost = new HttpPost("http://www.a-domain.com/foo/");
+//
+//// Request parameters and other properties.
+//            List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+//            params.add(new BasicNameValuePair("param-1", "12345"));
+//            params.add(new BasicNameValuePair("param-2", "Hello!"));
+//            httppost.setEntity(new UrlEncodedFormEntity(params, "UTF-8"));
+//
+////Execute and get the response.
+//            HttpResponse response = httpclient.execute(httppost);
+//            HttpEntity entity = response.getEntity();
+//
+//            if (entity != null) {
+//                InputStream instream = entity.getContent();
+//                try {
+//                    // do something useful
+//                } finally {
+//                    instream.close();
+//                }
+//            }
+//        }
 }
